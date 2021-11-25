755346f58ae08d15622c11f2070458b0d27d2dc0
Vanessa riquelme <vriquelmefe@gmail.com>
2021-11-24 22:11:19 -0300

    gif loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,12 +1,19 @@
+import loading from '../../assets/img/loading.gif'
+import './Loading.css'
+
 const Loading = () => {
-    return (
-        <div className="d-flex justify-content-center mt-4">
-            <button className="btn btn-dark" type="button" disabled>
-                <span className="spinner-border text-danger mr-4" role="status" aria-hidden="true"></span>
-                Cargando producto escogido...
-            </button>
-        </div>
-    );
+  return (
+    <div className=" container  text-center mt-4">
+
+      <div>
+        <img src={loading} className="imagen" alt="cargando" />
+      </div>
+      <div>
+        <p className="mr-4 text-pink font-weight-bold mt-3" role="status" aria-hidden="true">Cargando producto escogido...</p>
+
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
